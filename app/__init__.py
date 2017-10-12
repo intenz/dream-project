2657cae563b7db816aaa3d616c88fc0395da72b6
Viktor <_intenz_@ukr.net>
2017-10-12 12:27:54 +0300

    'finishLogin'

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,13 @@
 # third-party imports
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
 # local imports
 from config import app_config
 
-# db variable initialization
 db = SQLAlchemy()
 login_manager = LoginManager()
 
@@ -15,8 +15,8 @@ def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
+    Bootstrap(app)
     db.init_app(app)
-
     login_manager.init_app(app)
     login_manager.login_message = "You must be logged in to access this page."
     login_manager.login_view = "auth.login"
@@ -24,21 +24,13 @@ def create_app(config_name):
 
     from app import models
 
-
     from .admin import admin as admin_blueprint
     app.register_blueprint(admin_blueprint, url_prefix='/admin')
-
 
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
 
-
     from .home import home as home_blueprint
     app.register_blueprint(home_blueprint)
-
-
-    @app.route('/blueprint')
-    def blueprinteror():
-        return 'hi'
 
     return app
--- instance/config.py
+++ instance/config.py
@@ -1,3 +1,4 @@
 SECRET_KEY = 'p9Bv<3Eid9%$i01'
-SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+# SQLALCHEMY_DATABASE_URI = 'mysql://dt_admin:dt2017@localhost:3306/dreamteam_db'
+SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://dt_admin:dt2017@localhost:3306/dreamteam_db'
+SQLALCHEMY_TRACK_MODIFICATIONS = True
